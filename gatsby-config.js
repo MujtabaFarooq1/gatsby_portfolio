0c3b5079ca3ebb7e359e4aa30d5cfa0e6102822c
Mujtaba Farooq <mujtabafarooq730@gmail.com>
2022-03-03 00:02:05 +0500

    Initial Commit

---
+++ gatsby-config.js
@@ -0,0 +1,98 @@
+module.exports = {
+    siteMetadata: {
+        siteUrl: "http://localhost:8000/",
+        title: "MERN Stack Developer",
+        description: "Portfolio",
+        author: "Mujtaba Farooq",
+        siteLanguage: "en",
+        image: "banner.jpg",
+        titleTemplate: "",
+        twitterUsername: "@mujtaba",
+        getform_url: "http://localhost:8000/",
+        socials: [
+            {
+                id: 1,
+                title: "facebook",
+                path: "https://facebook.com",
+                icon: "Facebook",
+            },
+            {
+                id: 2,
+                title: "instagram",
+                path: "https://instagram.com",
+                icon: "Instagram",
+            },
+            {
+                id: 3,
+                title: "linkedin",
+                path: "https://linkedin.com",
+                icon: "Linkedin",
+            },
+        ],
+        contact: {
+            phone: "03248846007",
+            email: "mujtabafarooq730@gmail.com",
+        },
+    },
+    plugins: [
+        {
+            resolve: "gatsby-plugin-sass",
+            options: {
+                useResolveUrlLoader: {
+                    options: {
+                        sourceMap: true, //default is false
+                    },
+                },
+            },
+        },
+        "gatsby-plugin-image",
+        "gatsby-plugin-react-helmet",
+        "gatsby-plugin-sitemap",
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+        "gatsby-transformer-json",
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/assets/images`,
+                ignore: [`**/\.*`],
+            },
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `data`,
+                path: `${__dirname}/src/data`,
+                ignore: [`**/\.*`],
+            },
+        },
+        {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+                plugins: [
+                    {
+                        resolve: `gatsby-remark-images`,
+                        options: {
+                            maxWidth: 1200,
+                        },
+                    },
+                    "gatsby-remark-reading-time",
+                ],
+            },
+        },
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: "mujtaba",
+                short_name: "mujtaba",
+                theme_color: "#ff014f",
+                background_color: "#ffffff",
+                display: "standalone",
+                scope: "/",
+                start_url: "/",
+                icon: "src/assets/images/favicon.png",
+            },
+        },
+    ],
+};
